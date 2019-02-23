22e0f5f43179790e2a24fc39653181b195e77835
sean <seannguyen5696@gmail.com>
2019-02-23 18:29:23 +1100

    change 1

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <h1>This is home</h1>
+                <h1>This is home (1)</h1>
             </div>
         )
     }
